010fa25a38509bcbf91f9f4578150f39aadea7b0
holysideburns <jpettersson84@gmail.com>
2024-09-06 15:27:23 +0200

    Switched to headless browser.

--- tests/conftest.py
+++ tests/conftest.py
@@ -19,6 +19,7 @@ def page(browser):
 def browser_context_args(browser_context_args):
     return {
         **browser_context_args,
+        "headless": Config.HEADLESS,
         "viewport": {
             "width": 1920,
             "height": 1080,
