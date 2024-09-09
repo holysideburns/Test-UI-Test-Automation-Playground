b13641a0def8a198b8edede1d9c1237161647b19
holysideburns <jpettersson84@gmail.com>
2024-09-09 16:24:25 +0200

    Tried adding test cases to each test.

--- tests/test_click_page.py
+++ tests/test_click_page.py
@@ -8,6 +8,7 @@ import pytest
 from playwright.sync_api import expect
 from pages.click_page import ClickPage
 
+""" Test Scenario 1: Verify the page title is 'Click'. """
 @pytest.mark.click
 #@pytest.mark.skip(reason="Skipping this test for now.")
 def test_click_page_title(page) -> None:
@@ -15,11 +16,11 @@ def test_click_page_title(page) -> None:
     click_page.navigate()
     expect(click_page.get_title()).to_have_text("Click")
 
+""" Test Scenario 2: Verify the button can be clicked. """
 @pytest.mark.click
 #@pytest.mark.skip(reason="Skipping this test for now.")
 def test_emulated_mouse_click(page) -> None:
     click_page = ClickPage(page)
     click_page.navigate()
-    expect(click_page.get_button()).to_have_class('btn btn-primary')
     click_page.click_button()
     expect(click_page.get_button()).to_have_class('btn btn-success')
