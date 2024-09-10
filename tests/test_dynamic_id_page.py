92ca47d33e273f4ed98e27dc9bf2d5086ca7b96c
holysideburns <jpettersson84@gmail.com>
2024-09-10 14:36:20 +0200

    Refactored to use fixture

--- tests/test_dynamic_id_page.py
+++ tests/test_dynamic_id_page.py
@@ -8,18 +8,28 @@ import pytest
 from playwright.sync_api import expect
 from pages.dynamic_id_page import DynamicIdPage
 
-""" Test Scenario: Verify that the page title is 'Dynamic ID'. """
-@pytest.mark.dynamicid
-
-def test_dynamic_id_page_title(page) -> None:
+@pytest.fixture
+def dynamicid_page(page) -> DynamicIdPage:
     dynamicid_page = DynamicIdPage(page)
     dynamicid_page.navigate()
+    return dynamicid_page
+
+@pytest.mark.dynamicid
+def test_dynamic_id_page_title(dynamicid_page) -> None:
+    """
+    Test Scenario: Test Page Title
+        Given the user navigates to the 'Dynamic ID' page,
+        When the page is loaded,
+        Then the page title should be 'Dynamic ID'.
+    """
     expect(dynamicid_page.title).to_have_text("Dynamic ID")
 
-""" Test Scenario: Verify that the button can be clicked without using its ID. """
 @pytest.mark.dynamicid
-
-def test_dynamic_id_button(page) -> None:
-    dynamicid_page = DynamicIdPage(page)
-    dynamicid_page.navigate()
+def test_dynamic_id_button(dynamicid_page) -> None:
+    """
+    Test Scenario: Test Clicking Button Without Using ID
+        Given the user is on the 'Dynamic ID' page,
+        When the page is loaded,
+        Then the button should be clickable using a locator that is not ID.
+    """
     dynamicid_page.dynamicid_button.click()
