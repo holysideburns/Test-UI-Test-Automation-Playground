fdbe42b5eb4510c75c738705a7f2f9fe2e746c1e
holysideburns <jpettersson84@gmail.com>
2024-09-10 14:55:31 +0200

    Refactored to use fixture

--- tests/test_click_page.py
+++ tests/test_click_page.py
@@ -8,19 +8,29 @@ import pytest
 from playwright.sync_api import expect
 from pages.click_page import ClickPage
 
-""" Test Scenario: Verify that the page title is 'Click'. """
-@pytest.mark.click
-
-def test_click_page_title(page) -> None:
+@pytest.fixture
+def click_page(page) -> ClickPage:
     click_page = ClickPage(page)
     click_page.navigate()
+    return click_page
+
+@pytest.mark.click
+def test_click_page_title(click_page) -> None:
+    """
+    Test Scenario: Test Page Title
+        Given the user navigates to the 'Click' page,
+        When the page is loaded,
+        Then the page title should be 'Click'.
+    """
     expect(click_page.title).to_have_text("Click")
 
-""" Test Scenario: Verify the button can be clicked. """
 @pytest.mark.click
-
-def test_emulated_mouse_click(page) -> None:
-    click_page = ClickPage(page)
-    click_page.navigate()
+def test_emulated_mouse_click(click_page) -> None:
+    """
+    Test Scenario: Test Emulated Mouse Click
+        Given the user navigates to the 'Click' page,
+        When the page is loaded,
+        Then the user should be able to click the button.
+    """
     click_page.click_button()
     expect(click_page.button).to_have_class('btn btn-success')
