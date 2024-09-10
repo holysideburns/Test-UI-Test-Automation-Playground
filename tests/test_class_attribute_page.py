46359882579d1a8f71b2638c95f09b4cc207e3e7
holysideburns <jpettersson84@gmail.com>
2024-09-10 14:58:57 +0200

    Refactored to use fixture

--- tests/test_class_attribute_page.py
+++ tests/test_class_attribute_page.py
@@ -8,20 +8,29 @@ import pytest
 from playwright.sync_api import expect
 from pages.class_attribute_page import ClassAttributePage
 
-""" Test Scenario: Verify that the page title is 'Class Attribute'. """
-@pytest.mark.classattribute
-
-def test_class_attribute_page_title(page) -> None:
+@pytest.fixture
+def class_attribute_page(page) -> ClassAttributePage:
     class_attribute_page = ClassAttributePage(page)
     class_attribute_page.navigate()
+    return class_attribute_page
+
+@pytest.mark.classattribute
+def test_class_attribute_page_title(class_attribute_page) -> None:
+    """
+    Test Scenario: Test Page Title
+        Given the user navigates to the 'Class Attribute' page,
+        When the page is loaded,
+        Then the page title should be 'Class Attribute'.
+    """
     expect(class_attribute_page.title).to_have_text("Class Attribute")
 
-""" Test Scenario: Verify that the blue button can be click using the 'btn-primar' class. """
 @pytest.mark.classattribute
-
-def test_blue_button(page) -> None:
-    class_attribute_page = ClassAttributePage(page)
-    class_attribute_page.navigate()
+def test_blue_button(class_attribute_page) -> None:
+    """
+    Test Scenario: Test Text Input
+        Given the user is on the 'Text Input' page,
+        When the page is loaded,
+        Then the button should be clickable using the 'btn-primer' class.
+    """
     class_attribute_page.click_blue_button()
     expect(class_attribute_page.title).to_be_visible()
-    
